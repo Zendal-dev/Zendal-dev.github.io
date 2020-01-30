c1b9a8b48f5fc4863a0b65ce57263e63adc45b77
Zendal-dev <artzendal@gmail.com>
2020-01-30 12:30:49 +0200

    Abitu. Fixed slider

--- Aditu/js/app.js
+++ Aditu/js/app.js
@@ -4,10 +4,30 @@ $(function(){
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
-      // autoplay: true,
-      // autoplaySpeed: 2000,
-      easing: 'linear',
-      speed: 1000
+      speed: 1000,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      cssEase: 'linear',
+      responsive: [
+         {
+           breakpoint: 1180,
+           settings: {
+             slidesToShow: 3,
+             arrows: false,
+             dots: true,
+            }
+         },{
+            breakpoint: 816,
+            settings: {
+              slidesToShow: 2,
+             }
+          },{
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+             }
+          }
+      ]
    });
-  
+              
 });
