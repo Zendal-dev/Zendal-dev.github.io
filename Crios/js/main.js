abe597dbc433246762ce1f9077f5dcb6d48e5a73
Zendal-dev <artzendal@gmail.com>
2020-06-04 00:07:56 +0300

    ...

---
+++ Crios/js/main.js
@@ -0,0 +1,36 @@
+$(function () {
+
+   /* BURGER-MENU */
+   const $headerNav = $('.header-nav');
+   $headerNav.hide();
+
+   $('.header-burger').on('click', function () {
+      $(this).toggleClass('active');
+      $headerNav.fadeToggle(200);
+   });
+
+   /* SEARCH */
+   const $field = $('.header-search__field');
+
+   $('.header-search__field').hide();
+
+   $('.header-search').on('click', function () {
+      const $this = $(this);
+
+      if (!$this.is('.active')) {
+         $field.fadeIn(100).animate({
+            opacity: 1,
+            left: '-190'
+         });
+         setTimeout(() => $this.addClass('active'), 400)
+      } else {
+         $field.animate({
+            opacity: '0',
+            left: '0'
+         }, 500).fadeOut();
+         setTimeout(() => $this.removeClass('active'), 100);
+      }
+
+   });
+
+});
