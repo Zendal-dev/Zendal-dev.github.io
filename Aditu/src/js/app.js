cc13c8d27e1374b6781b2e9e7d6a1ada4505d86c
Zendal-dev <artzendal@gmail.com>
2020-07-29 21:05:27 +0300

    structure fixed

---
+++ Aditu/src/js/app.js
@@ -0,0 +1,33 @@
+$(function(){
+
+   $('.categories-slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      speed: 1000,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      cssEase: 'linear',
+      responsive: [
+         {
+           breakpoint: 1180,
+           settings: {
+             slidesToShow: 3,
+             arrows: false,
+             dots: true,
+            }
+         },{
+            breakpoint: 816,
+            settings: {
+              slidesToShow: 2,
+             }
+          },{
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 1,
+             }
+          }
+      ]
+   });
+              
+});
